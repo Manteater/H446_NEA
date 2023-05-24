d9b45f10656d13b258159e13d359798cbdc913d2
Manteater <gaming.anton@hotmail.com>
2023-05-24 16:22:59 +0100

    new level template

---
+++ Source/Actors/Player.gd
@@ -0,0 +1,97 @@
+extends KinematicBody2D
+
+export var speed: = 300#determines speed of player
+onready var anim_player: AnimationPlayer = get_node("AnimationPlayer")#fetches the animationplayer node
+onready var tree = get_tree()#fetches access to the node tree
+export onready var health = 3
+var player = null
+var _velocity = Vector2.ZERO
+
+
+func _ready():
+	player = tree.get_nodes_in_group("Player")[0]#fetches the player node to access certain attributes
+
+func _physics_process(delta: float) -> void:
+	if health == 0:
+		die()
+	#fetches the position vector of the cursor
+	var mouse = get_global_mouse_position()
+	#moves the player
+	move(delta)
+	#animates the players movements
+	animate(mouse)
+	
+	
+
+func get_direction() -> Vector2:#returns a vector2 for the direction of movement based on the input
+	return Vector2(
+		Input.get_action_strength("move_right")- Input.get_action_strength("move_left"),#value between 1 and -1
+	Input.get_action_strength("move_down")- Input.get_action_strength("move_up"))#value between 1 and -1
+		
+func calculate_move_velocity(
+	linear_velocity: Vector2,
+	direction: Vector2,
+	speed
+	) -> Vector2:
+		var out:=linear_velocity
+		#multiplies the direction by the speed of the player
+		out.x = speed*direction.x
+		out.y = speed*direction.y
+		return out
+
+func move(delta):
+	
+	var direction = get_direction()#gets the direction the player is moving in
+	_velocity = calculate_move_velocity(_velocity, direction,speed)#calculates the speed at which the player is moving
+	_velocity = move_and_slide(_velocity)#moves the player with in-built function
+
+	if Input.is_action_pressed("dash") and $dash_cooldown.time_left == 0:#if the dash key is pressed and cooldown has reset the player dashes
+		dash()
+
+func dash():
+	speed = speed*4#increases the speed of the player
+	immune(true)#makes theplayer immune to damage
+	#starts the timersS
+	$dash_timer.start()
+	$dash_cooldown.start()
+
+func _on_dash_timer_timeout():
+	speed=300#resets the speed of the player
+	immune(false)#immunity is false
+	
+
+func _on_hit_detector_body_entered(body):#if a body enters the player's hitbox
+	health -= 1
+	
+
+func animate(mouse):
+	if _velocity != Vector2.ZERO:#when player IS moving
+		if player.global_position.x < mouse.x:#if the mouse is to the right of the player
+			anim_player.play("run_right")
+		else:
+			anim_player.play("run_left") 
+	else:#when player isnt moving
+		if player.global_position.x < mouse.x:#if the mouse is to the right of the player
+			anim_player.play("idle_right")
+		else:
+			anim_player.play("idle_left")
+
+
+
+func immune(immunity)->void:
+	if immunity:
+		modulate.a = 0.5#makes the player slightly transparent to show the player that i frames are active
+		$hit_detector/bullet_detector.disabled = true#disables the player's hitbox
+	elif not immunity:
+		modulate.a = 1.0#makes the player look normal again
+		$hit_detector/bullet_detector.disabled = false#enables the player's hitbox
+
+func die():
+	anim_player.play("Death")#plays the death animation
+
+
+
+
+
+
+	
---
+++ Source/Actors/Stomper.gd
@@ -0,0 +1,77 @@
+extends KinematicBody2D
+#exports
+export var maxSpeed: int  = 200
+export var playerPath := NodePath()		#allows importing of the player scene
+
+#Nodes
+onready var animPlayer = $AnimationPlayer #access to animation player
+onready var player := get_node(playerPath)
+onready var agent = $NavigationAgent2D		# access to the navigation of the level
+onready var pathTimer = $pathTimer		#access to timer to update pathfinding
+onready var los = $RayCast2D		##acess to the Stompers line of sight
+
+#other
+var velocity := Vector2.ZERO
+var playerSpotted = false
+var collider = null
+var awoken = false
+
+
+func _ready():
+	playerSpotted = false		#by default player is not spotted
+	updatePathfinding()			#sets up the pathfinding loop
+	pathTimer.connect("timeout",self,"updatePathfinding") #calls the updatepathfinding function everytime the timer timesout
+
+func _physics_process(delta):		#function calls every frame of the game
+	animate()
+	checkPlayer()
+	
+	if playerSpotted:
+		move(delta)
+	else:
+		velocity = Vector2.ZERO
+	
+func animate():
+	if not awoken:#if the stomper hasnt seen the player then it sleeps
+			animPlayer.play("sleeping")
+	else:
+		if velocity != Vector2.ZERO:#if the stomper is moving
+			if velocity.x > 0:#if it is moving to the right
+				animPlayer.play("run_right")
+			else:#if it is moving to the left
+				animPlayer.play("run_left")
+		else:#if the stomper isnt moving then it plays the idle animation
+			animPlayer.play("idle_right")
+		
+	
+	
+	
+func move(delta):
+	if agent.is_navigation_finished():#if the navigation is completed itg cancels the function
+		return
+	if not awoken:#if the stomper hasn't awoken before then it will do so now as the player must have been spotted
+		awaken()
+	awoken = true
+	var direction := global_position.direction_to(agent.get_next_location()) #sets the direction to the desired position
+	var desiredVelocity := direction * maxSpeed #sets the speed of the stomper
+	var steering = (desiredVelocity - velocity)*delta*4.0 #sets the speed in relation to current speed
+	velocity += steering
+	velocity = move_and_slide(velocity)#actually moves the stomper on the screen
+	
+
+func awaken():
+	print("awakening")
+	animPlayer.play("Awake")
+	yield(get_tree().create_timer(0.5),"timeout")# pauses the stomper as the animation plays
+
+func updatePathfinding():
+	if playerSpotted:			#only sets a path when the player is spotted
+		agent.set_target_location(player.global_position)	#sets target location of the path
+
+func checkPlayer():
+	los.look_at(player.global_position)
+	collider = los.get_collider() #collidr is the hitbox of the raycast
+	if collider and collider.is_in_group("Player"):#if the line of sight is inside of the player
+		playerSpotted = true
+	else:
+		playerSpotted = false
