45d116477955791c7e9729b6a11b095c2fcd37c4
Manteater <gaming.anton@hotmail.com>
2024-03-27 12:52:37 +0000

    added upgrades to skillbuttons

--- Source/Tools/upgrade.gd
+++ Source/Tools/upgrade.gd
@@ -1,6 +1,7 @@
 extends Resource
 class_name Upgrade
 
+
 export var upgradeName: String
 export var upgradeType: String
 export var upgradeDescription: String
--- Source/UI/Buttons/skillButton.gd
+++ Source/UI/Buttons/skillButton.gd
@@ -4,9 +4,7 @@ class_name SkillNode
 onready var label = $Label
 onready var line = $Line2D
 var active = false
-export var upgradeType:= ""#this will determine what part of the player is being upgraded
-export var upgradeValue:= 0#this will determine how much this will be upgraded by
-export var upgradeCost= 1
+export var upgrade: Resource
 
 func _ready():
 	var position = rect_global_position
@@ -14,14 +12,16 @@ func _ready():
 	if get_parent().is_in_group("skillNodes"):#if it has a skillNode parent
 		line.add_point(position+size/2)#points at the centreof itself and it parent are added to the line which is automatically drawn
 		line.add_point(get_parent().rect_global_position+size/2)
-		#line.set_as_toplevel(true)
+		line.set_as_toplevel(true)
 		line.visible = false
+	$Cost.text = "Cost: "+str(upgrade.upgradeCost)
+	$Name.text = upgrade.upgradeName
 
-func _physics_process(delta):
-	line.visible = visible
+
+
 
 func _on_TextureButton_pressed():
-	if upgradeCost <= Global.characterSave.xpPoints:
+	if upgrade.upgradeCost <= Global.characterSave.xpPoints:
 		active = true
 	if active:
 		$Panel.show_behind_parent = true#this will make the button change color
@@ -31,10 +31,10 @@ func _on_TextureButton_pressed():
 			if skill.is_in_group("skillNodes") and active:
 				#if a child is a skillNode and the button is active then the child will be clickable
 				skill.disabled = false
-			match upgradeType:
-				"speed": Global.characterSave.speed += upgradeValue
-				"maxHealth": Global.characterSave.maxHealth += upgradeValue
-				"bulletDamage": Global.characterSave.bulletDamage += upgradeValue
-				"bulletSpeed": Global.characterSave.bulletSpeed += upgradeValue
-				"bulletAmount": Global.characterSave.bulletAmount += upgradeValue
+			match upgrade.upgradeType:
+				"speed": Global.characterSave.speed += upgrade.upgradeValue
+				"maxHealth": Global.characterSave.maxHealth += upgrade.upgradeValue
+				"bulletDamage": Global.characterSave.bulletDamage += upgrade.upgradeValue
+				"bulletSpeed": Global.characterSave.bulletSpeed += upgrade.upgradeValue
+				"bulletAmount": Global.characterSave.bulletAmount += upgrade.upgradeValue
 				
